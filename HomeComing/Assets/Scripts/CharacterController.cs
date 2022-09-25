872f9f75df2e16c4747dc0d30793eac3f4b51a62
Thacooo <59308946+TahaKoyuturk@users.noreply.github.com>
2022-09-25 21:21:25 +0300

    .

--- HomeComing/Assets/Scripts/CharacterController.cs
+++ HomeComing/Assets/Scripts/CharacterController.cs
@@ -1,28 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float moveSpeed;
     [SerializeField] private Animator animation;
-    bool go=false;
+    [SerializeField] private Transform winSpot;
+    private bool go=false;
     
     void Update()
     {
-        if (go)
-        {
-            animation.SetBool("isWalking", true);
-            rb.velocity = Vector3.forward * moveSpeed;
-        }
-        if (transform.position.y < -0.1f)
-        {
-            print("Game Over");
-        }
+        StartCoroutine(CharacterPlay());
     }
     public void Go()
     {
         go = true;
+    }
+    IEnumerator CharacterPlay()
+    {
+        while (true)
+        {
+            if (go)
+            {
+                animation.SetBool("isWalking", true);
+                rb.velocity = Vector3.forward * moveSpeed;
+                if (Mathf.Abs(winSpot.position.z - transform.position.z) < 0.1f)
+                {
+                    rb.velocity = Vector3.zero;
+                    animation.SetBool("isWin", true);
+                    yield return new WaitForSeconds(1.5f);
+                    SceneManager.LoadScene("Level1");
+                }
+            }
+            if (transform.position.y < -0.1f)
+            {
+                animation.SetBool("isFalling", true);
+                rb.velocity = Vector3.down * moveSpeed;
+                yield return new WaitForSeconds(2f);
+                SceneManager.LoadScene("Level1");
+                print("Game Over");
+            }
+        }
     }
 }
