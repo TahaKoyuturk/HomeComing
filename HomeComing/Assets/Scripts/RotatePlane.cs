301389f6610b212d30e254c2d05d56fd0bce1f82
Thacooo <Thacooo@users.noreply.github.com>
2022-09-25 04:44:58 +0300

    .

--- HomeComing/Assets/Scripts/RotatePlane.cs
+++ HomeComing/Assets/Scripts/RotatePlane.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
-public class Rotation : MonoBehaviour
+public class RotatePlane : MonoBehaviour
 {
 
     void Update()
